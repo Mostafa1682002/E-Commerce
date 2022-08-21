1c1d6d7b510133468f07505921fcf2e57c113864
Mostafa1682002 <ma9856603@gmail.com>
2022-08-21 02:21:38 +0200

    Frist Commit

---
+++ JS/main.js
@@ -0,0 +1,60 @@
+let menu=document.querySelector("#menu-bar");
+let navbar=document.querySelector(".navbar");
+menu.onclick=function(){
+    menu.classList.toggle("fa-times");
+    navbar.classList.toggle("show");
+}
+
+let allLinks=document.querySelectorAll(".navbar a");
+
+allLinks.forEach((e)=>{
+    e.onclick=function(){
+        allLinks.forEach((e)=>e.classList.remove("active"))
+        e.classList.add("active");
+    }
+})
+
+
+
+
+
+
+//Start Home Sections
+let allSliderCou=document.querySelectorAll(".slider-container");
+let index=0;
+
+function next(){
+    allSliderCou[index].classList.remove("visible");
+    index=(index+1)%allSliderCou.length;
+    allSliderCou[index].classList.add("visible");
+}
+function prev(){
+    allSliderCou[index].classList.remove("visible");
+    index=(index+1)%allSliderCou.length;
+    allSliderCou[index].classList.add("visible");
+}
+//End Home Sections
+
+//Start featured Sections
+let imgesSmall1=document.querySelectorAll('.small-img .small-img-1');
+let bigImge1=document.querySelector(".big-img .big-img-1");
+imgeClick(imgesSmall1,bigImge1);
+
+let imgesSmall2=document.querySelectorAll('.small-img .small-img-2');
+let bigImge2=document.querySelector(".big-img .big-img-2");
+imgeClick(imgesSmall2,bigImge2);
+
+let imgesSmall3=document.querySelectorAll('.small-img .small-img-3');
+let bigImge3=document.querySelector(".big-img .big-img-3");
+imgeClick(imgesSmall3,bigImge3);
+
+
+
+function imgeClick(imges,big){
+    imges.forEach((e)=>{
+        e.addEventListener("click",(ele)=>{
+            big.src=ele.target.src;
+        })
+    })
+}
+//End featured Sections
